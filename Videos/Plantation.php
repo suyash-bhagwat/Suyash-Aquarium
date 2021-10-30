afb436515d4036f2188f014b53d0cd7c815495a3
suyash-bhagwat <60439231+suyash-bhagwat@users.noreply.github.com>
2021-10-30 07:20:18 +0530

    Delete Videos directory

--- Videos/Plantation.php
+++
@@ -1,9 +0,0 @@
-<html lang="en">
-    <head>
-        <title>Plantation</title>
-        <link rel="stylesheet" href="..\CSS\imgvid.css">
-    </head>
-    <body>
-    <div>
-    </div>
-    </body>
